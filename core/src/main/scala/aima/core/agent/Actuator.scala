c2ad3ef7d242b16d9319e296f24a1ef07266ce11
Damien Favre <dam.favre@gmail.com>
2020-01-20 23:13:48 +0000

    Actuator and Sensor signal failure by returning Optional value

--- core/src/main/scala/aima/core/agent/Actuator.scala
+++ core/src/main/scala/aima/core/agent/Actuator.scala
@@ -1,23 +1,8 @@
 package aima.core.agent
 
-import aima.core.random.Randomness
-
 /**
   * @author Shawn Garner
   */
 trait Actuator[ENVIRONMENT, ACTION] {
-  def act(action: ACTION, e: ENVIRONMENT): ENVIRONMENT //could be Action => E => E
-}
-
-object UnreliableActuator {
-  def fromActuator[ENVIRONMENT, ACTION](
-      actuator: Actuator[ENVIRONMENT, ACTION]
-  )(
-      randomness: Randomness,
-      reliability: Int = 50
-  ): Actuator[ENVIRONMENT, ACTION] = new Actuator[ENVIRONMENT, ACTION] {
-    override def act(action: ACTION, e: ENVIRONMENT): ENVIRONMENT =
-      if (randomness.rand.nextInt(100) < reliability) actuator.act(action, e)
-      else e
-  }
+  def act(action: ACTION, e: ENVIRONMENT): Option[ENVIRONMENT]
 }
--- core/src/main/scala/aima/core/environment/vacuum/sensors.scala
+++ core/src/main/scala/aima/core/environment/vacuum/sensors.scala
@@ -6,11 +6,11 @@ import aima.core.agent.{Agent, Sensor}
   * @author Shawn Garner
   */
 class AgentLocationSensor(val agent: Agent[Vacuum, VacuumPercept, VacuumAction]) extends Sensor[Vacuum, VacuumPercept] {
-  def perceive(vacuum: Vacuum): VacuumPercept =
-    vacuum.map.getAgentLocation(agent).getOrElse(NoPercept)
+  def perceive(vacuum: Vacuum): Option[VacuumPercept] =
+    Some(vacuum.map.getAgentLocation(agent).getOrElse(NoPercept))
 }
 
 class DirtSensor(val agent: Agent[Vacuum, VacuumPercept, VacuumAction]) extends Sensor[Vacuum, VacuumPercept] {
-  def perceive(vacuum: Vacuum): VacuumPercept =
-    vacuum.map.getDirtStatus(agent).getOrElse(NoPercept)
+  def perceive(vacuum: Vacuum): Option[VacuumPercept] =
+    Some(vacuum.map.getDirtStatus(agent).getOrElse(NoPercept))
 }
